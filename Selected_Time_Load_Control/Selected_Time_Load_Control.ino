c482302954223bd492202e712ec26aea03554eb9
Rasel Ahmed <63443874+raselahamed613@users.noreply.github.com>
2023-10-29 00:20:43 +0600

    Implemention 7h delay circuit

--- Selected_Time_Load_Control/Selected_Time_Load_Control.ino
+++ Selected_Time_Load_Control/Selected_Time_Load_Control.ino
@@ -1,24 +1,23 @@
 #include <avr/sleep.h>
 
-const int relayPin = 13;  // Pin 0 for controlling the relay
+#define relayPin 0 // Pin 0 for controlling the relay
 
 unsigned long startTime;
-unsigned long delayDuration = 4L * 30L * 60L * 1000L;  // 7 hours in milliseconds
-//unsigned long delayDuration =   2L * 30L * 1000L;//7hours * 1h=60min * 1min=60sec * 1sec=1000ms
+unsigned long delayDuration = 7L * 60L * 60L * 1000L;  // 7 hours in milliseconds
+//unsigned long delayDuration =   1L * 10L * 60L * 1000L;//7hours * 1h=60min * 1min=60sec * 1sec=1000ms
 void setup() {
   pinMode(relayPin, OUTPUT);
   startTime = millis();
-  //  Serial.begin(9600);
+  //    Serial.begin(9600);
+  //    Serial.println(delayDuration);
 }
 
 void loop() {
   // Turn on the relay for 7 hours
-  digitalWrite(relayPin, HIGH);
-  //  delay(7 * 3600000);  // 7 hours in milliseconds
-  //  delay(5000);
-  //  Serial.println(startTime);
+  digitalWrite(relayPin, 0);
+  //    Serial.println(millis());
   if (millis() - startTime >= delayDuration) {
-    digitalWrite(relayPin, LOW);
+    digitalWrite(relayPin, 1);
     // Put the ATtiny13 to sleep to conserve power
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_mode();

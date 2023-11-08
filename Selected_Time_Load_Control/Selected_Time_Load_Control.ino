9bb050e96735d67285d2a9900077639f4a1a246c
Rasel Ahmed <63443874+raselahamed613@users.noreply.github.com>
2023-11-09 02:36:39 +0600

    Long time delay update

--- Selected_Time_Load_Control/Selected_Time_Load_Control.ino
+++ Selected_Time_Load_Control/Selected_Time_Load_Control.ino
@@ -1,23 +1,30 @@
 #include <avr/sleep.h>
 
-#define relayPin 0 // Pin 0 for controlling the relay
+// #define relayPin 0 // Pin 0 for controlling the relay
 
 unsigned long startTime;
-unsigned long delayDuration = 7L * 60L * 60L * 1000L;  // 7 hours in milliseconds
-//unsigned long delayDuration =   1L * 10L * 60L * 1000L;//7hours * 1h=60min * 1min=60sec * 1sec=1000ms
+// unsigned long delayDuration = 7L * 60L * 60L * 1000L;  // 7 hours in milliseconds
+unsigned long delayDuration = 5UL * 60UL * 60UL * 1000UL;  //5h delay
 void setup() {
-  pinMode(relayPin, OUTPUT);
+  // pinMode(relayPin, OUTPUT);
+  DDRB |= (1 << DDB0);  //PB5 = O/P
   startTime = millis();
   //    Serial.begin(9600);
   //    Serial.println(delayDuration);
+  PORTB |= (1 << PB0);  //PB5 = HIGH
+  delay(3000);
+  PORTB &= ~(1 << PB0);  //PB5 = LOW
+  delay(2000);
 }
 
 void loop() {
   // Turn on the relay for 7 hours
-  digitalWrite(relayPin, 0);
+  // digitalWrite(relayPin, 0);
   //    Serial.println(millis());
   if (millis() - startTime >= delayDuration) {
-    digitalWrite(relayPin, 1);
+    // digitalWrite(relayPin, 1);
+    PORTB |= (1 << PB0);  //PB5 = HIGH
+    delay(500);
     // Put the ATtiny13 to sleep to conserve power
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_mode();
